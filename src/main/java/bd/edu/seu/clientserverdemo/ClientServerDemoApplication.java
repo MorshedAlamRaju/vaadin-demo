5629cb88d3f21d74f14473cc53ea06fa8f525859
Raju <maraju88@yahoo.com>
2019-09-05 22:19:04 +0600

    first commit

---
+++ src/main/java/bd/edu/seu/clientserverdemo/ClientServerDemoApplication.java
@@ -0,0 +1,13 @@
+package bd.edu.seu.clientserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/bd/edu/seu/clientserverdemo/ui/LoginView.java
@@ -0,0 +1,5 @@
+package bd.edu.seu.clientserverdemo.ui;
+import com.vaadin.flow.component.dialog.Dialog;
+
+public class LoginView extends Dialog {
+}
